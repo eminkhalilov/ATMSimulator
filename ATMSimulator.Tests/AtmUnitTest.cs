72eb9dea154c1017af6fb21a5b610249b1375d77
Emin <Emin@DESKTOP-GNURETT>
2021-07-13 09:12:37 +0400

    uncomitted method

--- ATMSimulator.Tests/AtmUnitTest.cs
+++ ATMSimulator.Tests/AtmUnitTest.cs
@@ -1,4 +1,5 @@
 using ATMSimulator.State;
+using ATMSimulator.States;
 using System;
 using System.Collections.Generic;
 using Xunit;
@@ -24,6 +25,16 @@ namespace ATMSimulator.Tests
             var amount = 10;
             var actual = hasCardState.WithdrawMoney(amount);
             var expected = new Money { Amount = amount, Notes = new Dictionary<PaperNote, int>() { { new PaperNote { Amount = amount, Name = "€" }, 0 } } };
+            Assert.Equal(expected.Amount, actual.Amount);
+            Assert.Equal(expected.Notes, actual.Notes);
+        }
+
+        [Fact]
+        public void ReturnCard_WithDrawal()
+        {
+            var noCardState = new NoCardState();
+            var actual = noCardState.WithdrawMoney(10);
+            var expected = new Money { Amount = 0, Notes = new Dictionary<PaperNote, int>() { { new PaperNote { Amount = 0, Name = "€" }, 0 } } };
             Assert.Equal(expected.Amount, actual.Amount);
             Assert.Equal(expected.Notes, actual.Notes);
         }
